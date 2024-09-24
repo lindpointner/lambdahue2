3c02046392c70d18d808fd0da4126610eace14b7
lindpointner <LindpointnerT22048@sus.htl-grieskirchen.at>
2024-09-24 20:22:25 +0200

    methodes included

--- lambdA/src/JavamitForEach.java
+++ lambdA/src/JavamitForEach.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class JavamitForEach {
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavamitForEach {
+    ArrayList<String> liste = new ArrayList<>();
+
+    public void showWithForEach(){
+        for (int i = 0; i < liste.size(); i++){
+            System.out.println(liste.get(i));
+        }
+    }
+
+    public void showWithOldForEach(){
+        liste.forEach(System.out::println);
+    }
+
+
 }
---
+++ lambdA/src/NumberTester.java
@@ -0,0 +1,17 @@
+public class NumberTester {
+    public NumberTester(String fileName) {
+        //TODO
+    }
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester =primeTester;
+    }
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester =palindromeTester;
+    }
+    public void testFile() {
+        //TODO
+    }
+}
