52d37ebdc19de9f1c505d2117032a12922c80bb3
lindpointner <LindpointnerT22048@sus.htl-grieskirchen.at>
2024-09-29 17:15:06 +0200

    RationalCalculator is finished

---
+++ lambdA/src/RationalCalculator.java
@@ -0,0 +1,25 @@
+public class RationalCalculator extends AbstractCalculator{
+
+    public RationalCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation mutliply, CalculationOperation devide){
+        super(add, subtract, mutliply, devide);
+    }
+    @Override
+    public Number add(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return null;
+    }
+}
