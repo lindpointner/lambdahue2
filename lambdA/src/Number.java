c594256001bfd47032aebca09d54a2db6fc9942d
lindpointner <LindpointnerT22048@sus.htl-grieskirchen.at>
2024-09-30 08:52:19 +0200

    Number is finished finished

--- lambdA/src/Number.java
+++ lambdA/src/Number.java
@@ -1,6 +1,10 @@
 public class Number {
     private double a;
     private double b;
+    public Number(double a, double b){
+        this.a = a;
+        this.b = b;
+    }
 
     public double getA() {
         return a;
